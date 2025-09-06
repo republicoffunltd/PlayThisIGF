4d70dd997647db8525a2a7a4891376de7d5f9bad
marcosventura1986 <82414153+marcosventura1986@users.noreply.github.com>
2025-09-06 14:27:08 +0100

    First Commit
    Intial website.

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Simple JS placeholder for future features
+// Example: Mobile menu toggle
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Play This IGF website loaded.");
+});
